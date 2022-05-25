f0f17ff39fc359875f0effbd77bcd191b9137324
dritter44 <dreither44@gmail.com>
2022-05-25 13:50:34 -0500

    first commit push

--- js/factorial.js
+++ js/factorial.js
@@ -1,2 +1,13 @@
 exports.factorial = function(num) {
-};
+        if(num === 0){
+            return 1
+        }
+        else if (num === 1){
+          return num;
+        } else {
+          return num * this.factorial(num - 1)
+        }
+      }
+    
+
+
